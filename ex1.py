fdd0703a5523848022030184f4753370094c7b27
Aldrin Thomas <aldrinthomas300@gmail.com>
2022-07-20 10:56:18 +0530

    ex1.py added

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#EXERCISE 1: A GOOD FIRST PROGRAM
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said"do not touch this.')
+
