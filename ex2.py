c75946c2163e54d9fee879902dec172f7f80b295
Aldrin Thomas <aldrinthomas300@gmail.com>
2022-07-20 15:37:50 +0530

    directory added

--- ex2.py
+++
@@ -1,9 +0,0 @@
-#Comments and Pound characters(octothorpe)
-
-#Anything which is writing after '#' character is not taken by the python
-
-print("But, you can print this")
-
-#Not this
-
-print("I'm the God now")
