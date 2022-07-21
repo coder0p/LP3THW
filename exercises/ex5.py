efd2418791084f434ca94fc2c575095560d1d8d7
Aldrin Thomas <aldrinthomas300@gmail.com>
2022-07-21 11:08:04 +0530

    ex5.py added

---
+++ exercises/ex5.py
@@ -0,0 +1,22 @@
+#Exercise :4
+
+#More variables and Printing
+
+my_name = 'Aldrin Thomas'
+my_age = 23 #yes that's true
+my_height = 165 # in centimeters
+my_weight = 95 #Kgs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's too heavy.")
+print(f"He's got {my_eyes} eyes and  {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the food he eats.")
+
+#this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If we add his age, height, and weight we get {total}.")
